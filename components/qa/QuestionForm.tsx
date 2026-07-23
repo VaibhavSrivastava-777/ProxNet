4e60649b2d98793656a02600f8f1c6457b19e199
VaibhavSrivastava-777 <srivastava.swatipandya@gmail.com>
2026-07-24 00:21:31 +0530

    fix: import useRef in QuestionForm

--- components/qa/QuestionForm.tsx
+++ components/qa/QuestionForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useRouter } from "next/navigation";
 import { LocationPicker } from "@/components/map/LocationPicker";
 import { LocationAutocomplete } from "@/components/map/LocationAutocomplete";
